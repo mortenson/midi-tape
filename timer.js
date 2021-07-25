2c796a82c7976119ceafa66b7dee3c44bae3d881
Sam Mortenson <samuel@mortenson.coffee>
2021-07-25 07:55:50 -0700

    Added function to track offset performance during development.

--- timer.js
+++ timer.js
@@ -2,6 +2,13 @@ var lastTick = 0;
 var ppq = 48;
 var bpm = 110;
 var tickRate = 60000 / (bpm * ppq);
+var nums = [];
+
+function debugTiming(offset) {
+  nums.push(offset);
+  var average = nums.reduce((a, b) => a + b) / nums.length;
+  console.log("Average offset ", average);
+}
 
 function tick() {
   if (lastTick === 0) {
