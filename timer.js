1ba7878a71722a2d991850c35d94732853d2c77f
Sam Mortenson <samuel@mortenson.coffee>
2022-06-04 11:30:56 -0700

    Remove unused offset variables.

--- timer.js
+++ timer.js
@@ -1,25 +1,10 @@
-var lastTick = 0;
 var ppq = 24;
 var bpm = 110;
 var tickRate = 60000 / (bpm * ppq);
-var nums = [];
-var numIndex = 0;
-
-function debugTiming(offset) {
-  nums[numIndex % 1000] = offset;
-  numIndex++;
-  var average = nums.reduce((a, b) => a + b) / nums.length;
-  console.log("Average offset ", average);
-}
 
 function tick() {
-  let now = performance.now();
-  if (lastTick === 0) {
-    lastTick = now;
-  }
   postMessage({});
   setTimeout(tick, tickRate);
-  lastTick = now;
 }
 
 tick();
